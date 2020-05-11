184d54dac2d8974571e782950302baf93436483c
Monica <moparra78@uan.edu.co>
2020-05-10 21:49:41 -0500

    primera entrega

---
+++ app/src/main/java/com/example/cuidadores/Main2Activity.kt
@@ -0,0 +1,34 @@
+package com.example.cuidadores
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ListView
+
+class Main2Activity : AppCompatActivity() {
+
+    /*ListView lista1
+
+    String[][] datos1 = {
+        {"mitituloa", "midescriciona"}
+        {"mititulob", "midescricionb"}
+
+    }
+
+    int[] datosicono1 = {R.drawable.iconoantiguedad, R.drawable.iconofase, R.drawable.iconomedicacion};
+*/
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+
+        /*lista1 = (ListView) findViewById(R.id.lista1)
+
+        lista1.setAdapter(new Adaptador(this, datos1, datosicono1))
+
+         */
+
+
+    }
+}
---
+++ app/src/main/java/com/example/cuidadores/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.cuidadores
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val siguiente= findViewById<Button>(R.id.btnsiguiente1)
+
+        siguiente.setOnClickListener {
+
+            val intent = Intent( this@MainActivity, Main2Activity::class.java)
+
+            startActivity(intent)    }
+
+    }
+}
